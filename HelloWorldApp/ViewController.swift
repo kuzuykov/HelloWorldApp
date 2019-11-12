c1f4a289baf200a1f98a23b8793b33541fae09f3
Константин Кузюков <Kuzuykov@outlook.com>
2019-11-12 17:28:39 +0300

    Initial Commit

---
+++ HelloWorldApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorldApp
+//
+//  Created by Константин Кузюков on 11.11.2019.
+//  Copyright © 2019 Константин Кузюков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
